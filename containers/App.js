5777519cc0d87270823cfa467f2c0b2c0275bfcb
Peter Lee <peterlee@lyft.com>
2018-03-19 00:15:26 -0700

    basic template to add images.

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
+import { Row, Column } from 'react-foundation';
 
 export default class App extends Component {
     render () {
-        return <p>Peter's test website </p>
+        return (
+            <div>
+                <p>Peter's test website </p>
+                <img src="./images/fauna.jpg" className="img-thumbnail" />
+            </div>
+        )
     }
 }
