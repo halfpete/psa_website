c3c58f66b3c3b2e90d39dcb837c6fba758f9b17c
Peter Lee <peterlee@lyft.com>
2018-03-18 23:39:13 -0700

    setup for peter's website

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+	entry: [
+		path.join(parentDir, 'index.js')
+    ],
+    module: {
+        // Bundles source files.
+		loaders: [{
+			test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},{
+				test: /\.less$/,
+				loaders: ["style-loader", "css-loader", "less-loader"]
+			}
+		]
+    },
+    // Where the bundle.js file gets outputted.
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    // dev server configurations.
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
