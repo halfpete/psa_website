82a6f9c1e8bf2263b0ebbb618176b1e6665bfb0a
Peter Lee <peterlee@lyft.com>
2018-04-02 22:49:38 -0700

    working locally with images. Not formatted (transitioning from basic fb react to old website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Row, Column } from 'react-foundation';
+import americana from './images/americana.png';
+import fauna from './images/fauna.jpg';
+import untitled_1 from './images/Untitled-1.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div class="header">
+          <h1 id="peter_ahn">
+            <a href="">Peter Ahn</a>
+          </h1>
+          <h1 id="about">
+            <a href="">About</a>
+          </h1>
+          <h1 id="contact">
+            <a href="">Contact</a>
+          </h1>
+        </div>
+        <body>
+          <div>
+            <img src={americana} className="wallpaper_header" alt="americana" />
+          </div>
+          <div class="row" id="grid_padding">
+            <div class="column">
+              <img src={untitled_1} className="grid_image" alt="untitled_1" />
+              <img src={fauna} className="grid_image" alt="fauna" />
+              <img src={fauna} className="grid_image" alt="fauna" />
+            </div>
+            <div class="column">
+              <img src={untitled_1} className="grid_image" alt="untitled_1" />
+              <img src={fauna} className="grid_image" alt="fauna" />
+              <img src={fauna} className="grid_image" alt="fauna" />
+            </div>
+          </div>
+        </body>
       </div>
     );
   }
