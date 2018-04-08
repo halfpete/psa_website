74df5fc9df596e1b90fc80c52a00a330457f7ebf
Peter Lee <peterlee@lyft.com>
2018-04-08 16:55:41 -0700

    fixed up website for peter's presentation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,18 @@ import { Link } from 'react-router-dom';
 import americana from './images/americana.png';
 import fauna from './images/fauna.jpg';
 import untitled_1 from './images/Untitled-1.jpg';
+import book10 from './images/book/Book master10.png';
+import book11 from './images/book/Book master11.png';
+import book12 from './images/book/Book master12.png';
+import book13 from './images/book/Book master13.png';
+import book14 from './images/book/Book master14.png';
+import book15 from './images/book/Book master15.png';
+import book16 from './images/book/Book master16.png';
+import book17 from './images/book/Book master17.png';
+import book18 from './images/book/Book master18.png';
+import book19 from './images/book/Book master19.png';
+import book20 from './images/book/Book master20.png';
 import './App.css';
-//<div className="body">
-//<div className="row" id="grid_padding">
-//<div className="column">
-//<img src={untitled_1} id="grid_image" alt="untitled_1" />
-//<img src={fauna} id="grid_image" alt="fauna" />
-//<img src={fauna} id="grid_image" alt="fauna" />
-//</div>
-//<div className="column">
-//<img src={untitled_1} id="grid_image" alt="untitled_1" />
-//<img src={fauna} id="grid_image" alt="fauna" />
-//<img src={fauna} id="grid_image" alt="fauna" />
-//</div>
-//</div>
-//</div>
 
 class App extends Component {
   render() {
@@ -41,10 +38,28 @@ class App extends Component {
             </Link>
           </h1>
         </div>
-        <div>
-          <img src={americana} id="americana" alt="americana" />
-          <img src={americana} id="americana2" alt="americana" />
-          <img src={americana} id="americana3" alt="americana" />
+        <div className="body">
+          <img src={fauna} id="americana" alt="americana" />
+          <div className="row" id="grid_padding">
+            <div className="column">
+              <img src={book14} id="grid_image" alt="untitled_1" />
+              <img src={americana} id="grid_image" alt="fauna" />
+              <img src={book15} id="grid_image" alt="fauna" />
+              <img src={book16} id="grid_image" alt="fauna" />
+            </div>
+            <div className="column">
+              <img src={book17} id="grid_image" alt="untitled_1" />
+              <img src={book18} id="grid_image" alt="fauna" />
+              <img src={book19} id="grid_image" alt="fauna" />
+              <img src={book20} id="grid_image" alt="fauna" />
+            </div>
+            <div className="column">
+              <img src={book10} id="grid_image" alt="untitled_1" />
+              <img src={book11} id="grid_image" alt="fauna" />
+              <img src={book12} id="grid_image" alt="fauna" />
+              <img src={book13} id="grid_image" alt="fauna" />
+            </div>
+          </div>
         </div>
         <div className="footer">
           <div>Peter Ahn</div>
