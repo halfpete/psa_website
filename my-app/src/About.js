456946326ebb1c54cc1a4dba9dbc10e3227e3dbf
Peter Lee <peterlee@lyft.com>
2018-04-10 22:27:39 -0700

    fixed about me page links

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -12,13 +12,22 @@ class About extends Component {
         <Link to="/">
           <img src={about_name} id="about_name" alt="title" />
         </Link>
+        <div className="about_links">
+          <Link to="/work" id="first_sub_header">
+            Work
+          </Link>
+          <Link to="/contact" id="second_sub_header">
+            Contact
+          </Link>
+        </div>
         <div>
           <img src={about_me} id="about_picture" alt="about_me" />
         </div>
         <div id="about_text">
           Peter Ahn is an artist and designer based in Los Angeles. He holds a
           BFA in Illustration from Art Center College of Design. In his spare
-          time he enjoys visiting arboretums and dreams of being famous.
+          time he enjoys visiting arboretums, tending to his cherry blossom
+          tree, and dreams of being famous.
         </div>
         <div id="resume">
           <a href={resume} target="_blank">
